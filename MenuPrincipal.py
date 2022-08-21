2b78f929c1a84caa2276d89f9b3ff263b0ed5921
KevinPalaciosQ <3001877180101@ingenieria.usac.edu.gt>
2022-08-20 22:39:25 -0600

    Proyecto Finalizado, Faltan Manuales y Ejecutable

--- MenuPrincipal.py
+++ MenuPrincipal.py
@@ -1,18 +1,11 @@
-
-from ast import Delete, Lambda
-from distutils.cmd import Command
-from optparse import Option
-from os import remove
-from re import A
 import tkinter
 from tkinter import *
 from tkinter import font
 from tkinter import ttk
 import tkinter as tk
 from tkinter.messagebox import showwarning
-from tkinter import Button, scrolledtext, filedialog, messagebox
+from tkinter import Button, filedialog, messagebox
 from tkinter import filedialog
-import turtle
 from types import NoneType
 from Credito import Creditos
 ventana=None
@@ -40,6 +33,9 @@ txtmostrarcreeditos=None
 txtmostrarestado=None
 txtEliminar=None
 global botongestion
+lblXX=None
+lblXX1=None
+lblXX2=None
 creditos = []
 arreglo=[]
 def RetornarPrincipal():
@@ -77,8 +73,6 @@ def RetornarMostrar():
 def obtenerruta():
     ruta = filedialog.askopenfilename(title='Cargar Archivo', filetypes = (("Text files", "*.lfp*"), ("all files", "*.*")))
     return ruta
-
-
 def CargardeArchivo():
     #VENTANA
     global VentanaP
@@ -110,22 +104,20 @@ def leerArchivo():
     global lblespacio 
     global creditos
     global credito
+    global data
     ruta = obtenerruta()
     if ruta != "":
         archivo = open(ruta,"r",encoding="utf8")   
         contenido = archivo.read()
         lblespacio.configure(text=""+ruta)
-        print(contenido)
-        #creditos = []
         contenido = contenido.split('\n')
         for contenidoo in contenido:
             data= contenidoo.split(",")
-            credito= Creditos(data[0], data[1],  data[3], data[4],data[5],data[6].rstrip("\n"))
-            agregarprerre(data[2], credito)
-            creditos.append(credito)
-            #print(credito)
-            print (data)
-        
+            if ExisteCurso(data[0])==False:
+                credito= Creditos(data[0], data[1],  data[3], data[4],data[5],data[6].rstrip("\n"))
+                agregarprerre(data[2], credito)
+                creditos.append(credito)
+                print (data)
         messagebox.showinfo("Success","Archivo cargado")
         return creditos
     else:
@@ -135,7 +127,12 @@ def agregarprerre(prerre, cursoo):
         press = prerre.split(",")
         for pe in press:
             cursoo.prerrequisitos.append(pe)
-
+def ExisteCurso(codigo):
+    global creditos
+    for iterador in creditos:
+        if iterador.codigo==codigo:
+            return True
+    return False
 def Gestionar():
     global VentanaP
     global ventanacurso
@@ -203,8 +200,10 @@ def Gestionar1():
     botonmostrarespecifico.place(x=335,y=370)
     ventanacurso.mainloop()
 def ConteoDeCreditos():
+
     global VentanaP
     global ventanaConteo
+    global lblXX, lblXX1,lblXX2,conteo1,conteo2,txtObligatorios,txtDelSemestre
     VentanaP.destroy()
     #VENTANA
     ventanaConteo = tkinter.Tk()
@@ -235,10 +234,10 @@ def ConteoDeCreditos():
     txtObligatorios.place(x="500",y="120",width="60",height="30")
     lblsemestre= Label(ventanaConteo,text="Semestre",font="Cambria 22", fg="blue violet", bg="sky blue")
     lblsemestre.place(x=70,y=150)
-    conteo1 = ttk.Spinbox(from_=0, to=12)
+    conteo1 = ttk.Spinbox(from_=0, to=10, )
     conteo1.place(x=190, y=163, width=50, height=20)
     #BOTON PARA CONTAR
-    botoncontar = Button(ventanaConteo,text="Contar",font="Cambria 10", fg="gray", bg="#e4ade4",relief="groove")
+    botoncontar = Button(ventanaConteo,text="Contar",font="Cambria 10", fg="gray", bg="#e4ade4",relief="groove",command=ConteoN)
     botoncontar.place(x=250,y=160)
     lblCreditosDelSemestre= Label(ventanaConteo,text="Créditos del Semestre:",font="Cambria 22", fg="blue violet", bg="sky blue")
     lblCreditosDelSemestre.place(x=10,y=185)
@@ -246,12 +245,62 @@ def ConteoDeCreditos():
     txtDelSemestre.place(x="300",y="190",width="60",height="30")
     lblsemestre1= Label(ventanaConteo,text="Semestre",font="Cambria 22", fg="blue violet", bg="sky blue")
     lblsemestre1.place(x=70,y=220)
-    conteo2 = ttk.Spinbox(from_=0, to=12)
+    conteo2 = ttk.Spinbox(from_=0, to=10)
     conteo2.place(x=190, y=235, width=50, height=20)
     #BOTON PARA CONTAR
-    botoncontar2 = Button(ventanaConteo,text="Contar",font="Cambria 10", fg="gray", bg="#e4ade4",relief="groove")
+    botoncontar2 = Button(ventanaConteo,text="Contar",font="Cambria 10", fg="gray", bg="#e4ade4",relief="groove",command=CreditosSemestre)
     botoncontar2.place(x=250,y=235)
+    botonEstado= Button(ventanaConteo,text=" Estado",font="Cambria 18", fg="gray", bg="#e4ade4",relief="groove",bd=9, command=ConteoGeneral)
+    botonEstado.place(x=520,y=20)
     ventanaConteo.mainloop()
+#Método Para Contar Hasta Semestre N
+def ConteoN():
+    global conteo1
+    obligatorios=0
+    global creditos
+    conteo1.get()
+    print(conteo1.get())
+    for iterador  in range(len(creditos)):
+        if int(creditos[iterador].semestre)<=int(conteo1.get()) and creditos[iterador].obligatorio=="1":
+            obligatorios +=int(creditos[iterador].creditos)
+    txtObligatorios.insert(tk.INSERT, obligatorios)
+    txtObligatorios.config(state="disabled")
+#Método Para Contar Creditos del Semestre
+def CreditosSemestre():
+    global conteo2
+    semestre=0
+    conteo2.get()
+    print(conteo2.get())
+    for iterador  in range(len(creditos)):
+        if int(creditos[iterador].semestre)==int(conteo2.get()) and (creditos[iterador].estado=="1" or creditos[iterador].estado=="0" or creditos[iterador].estado=="-1"):
+            semestre +=int(creditos[iterador].creditos)
+    txtDelSemestre.insert(tk.INSERT, semestre)
+    txtDelSemestre.config(state="disabled")
+
+#Método para Contar Aprobado, Cursado y Pendiente
+def ConteoGeneral():
+    #Contadores Inicializados en 0
+    global creditos
+    global lblXX,lblXX1,lblXX2
+    aprobados=0
+    cursando=0
+    pendiente=0
+    for iterador  in range(len(creditos)):
+        if creditos[iterador].estado=="0":
+            aprobados +=int(creditos[iterador].creditos)
+        if creditos[iterador].estado=="1":
+            cursando +=int(creditos[iterador].creditos)
+        if creditos[iterador].estado=="-1" and creditos[iterador].obligatorio=="1":
+            pendiente +=int(creditos[iterador].creditos)
+    print(aprobados)
+    print(cursando)
+    print(pendiente)
+    estado_aprobado=(str(aprobados))
+    estado_cursado=(str(cursando))
+    estado_pendiente=(str(pendiente))
+    lblXX.configure(text=""+estado_aprobado)
+    lblXX1.configure(text=""+estado_cursado)
+    lblXX2.configure(text=""+estado_pendiente)
 def Listar():
     global ventanacurso
     global ventanaListar
@@ -320,33 +369,19 @@ def Eliminar():
 def DeletCourse():
     global txtEliminar
     global creditos
-#    if (txtEliminar.get()!= "" ):
-#        for i in range( len(creditos)):
-#            if (creditos[i].codigo)==txtEliminar.get():
-#                creditos.pop(i)
-#                #creditos.remove(i)
-#                return creditos
-#        messagebox.showinfo("Success","Se Eliminó con Éxito el curso")
-#    elif (txtEliminar.get()!=creditos[i].codigo):            
-#        messagebox.showinfo("Warning","No se Encontró el Curso")
     variable =True
-    #if (txtEliminar.get()!= "" ):
     for i in range( len(creditos)):
         if (creditos[i].codigo)==txtEliminar.get():
-                
             creditos.pop(i)
             variable==False
-                #variable==False
-                #creditos.remove(i)
             messagebox.showinfo("Success","Se Eliminó con Éxito el curso")
             return creditos
-        
     else:
         pass
-    if variable==True:
-        messagebox.showinfo("Warning","No Se Encontró el curso")
-    else:
-        pass
+    if variable==True and txtEliminar.get()!="":
+        messagebox.showinfo("Warning","No Se Encontró el curso ")
+    elif txtEliminar.get()=="":
+        messagebox.showinfo("Warning","Por favor llene el campo")
 def Mostrar():
     global ventanacurso
     global ventaamostrar
@@ -389,10 +424,10 @@ def Mostrar():
     #ESPACIOS PARA ESCRIBIR
     txtmostrarcodigo=ttk.Entry(ventaamostrar,font="Cambria 22")
     txtmostrarcodigo.place(x="185",y="20",width="600",height="30")
-    txtmostrarnombre=ttk.Entry(ventaamostrar,font="Cambria 22")
+    txtmostrarnombre=ttk.Entry(ventaamostrar,font="Cambria 18")
     txtmostrarnombre.place(x="185",y="55",width="600",height="30")
     #txtmostrarnombre.config(state="disabled")
-    txtmostrarprerrequisito=ttk.Entry(ventaamostrar,font="Cambria 22")
+    txtmostrarprerrequisito=ttk.Entry(ventaamostrar,font="Cambria 18")
     txtmostrarprerrequisito.place(x="185",y="90",width="600",height="30")
     txtmostrarsemestre=ttk.Entry(ventaamostrar,font="Cambria 22")
     txtmostrarsemestre.place(x="185",y="125",width="600",height="30")
@@ -412,7 +447,6 @@ def ShowCourse():
     global txtmostrarcreeditos
     global txtmostrarestado
     global creditos
-    #if (txtmostrarcodigo.get()!= "" ):
     bandera =True
     for i in range( len(creditos)):
         if (creditos[i].codigo)==txtmostrarcodigo.get():
@@ -433,10 +467,10 @@ def ShowCourse():
             return creditos
         else:
             pass
-    if bandera==True:
+    if bandera==True and txtmostrarcodigo.get()!="":
         messagebox.showinfo("Warning","No Se Encontró el curso")
-    else:
-        pass
+    elif txtmostrarcodigo.get()=="":
+        messagebox.showinfo("Warning","Por favor llene el campo")
 
 def AgregaCurso():
     global txtcode
@@ -447,6 +481,7 @@ def AgregaCurso():
     global txtcredits
     global txtestate
     global arreglo
+    global data
     print(txtcode.get())
     print(txtname.get())
     print(txtprerequest.get())
@@ -454,13 +489,19 @@ def AgregaCurso():
     print(txtoption.get())
     print(txtcredits.get())
     print(txtestate.get())
+    for i in range( len(creditos)):
+        if (creditos[i].codigo)==txtcode.get():
+            #creditos.pop(i)
+            messagebox.showinfo("Success","Este curso ya existe")
+            return creditos
     if (txtcode.get()!= "" and txtname.get()!="" and txtprerequest.get()!="" and txtsemester.get()!="" and txtoption.get()!="" and txtcredits.get()!="" and txtestate.get()!=""):
         niu=Creditos(txtcode.get(),txtname.get(),txtsemester.get(),txtoption.get(),txtcredits.get(),txtestate.get())
         agregarprerre(txtprerequest.get(), niu)
         creditos.append(niu) 
         messagebox.showinfo("Success","Se agregó el Curso")
-    elif (txtcode.get()== "" and txtname.get()=="" and txtprerequest.get()=="" and txtsemester.get()=="" and txtoption.get()=="" and txtcredits.get()=="" and txtestate.get() ==""):
-        messagebox.showinfo("Warning","No se cargó ningun Curso")
+        return creditos
+    elif (txtcode.get()== "" and txtname.get()=="" and txtprerequest.get()=="" and txtsemester.get()=="" and txtoption.get()=="" and txtcredits.get()=="" and txtestate.get()==""):
+        messagebox.showinfo("Warning","Por favor llene los campos")
 
 def Agregar():
     global ventanacurso
@@ -582,19 +623,25 @@ def Edicion():
     global txteditaropcionalidad
     global txteditarcreeditos
     global txteditarestado
-    if (txteditarcodigo.get()!= "" and txteditarnombre.get()!="" and txteditarprerrequisito.get()!="" and txteditarsemestre.get()!="" and txteditaropcionalidad.get()!="" and txteditarcreeditos.get()!="" and txteditarestado.get()!=""):
-        variable1=Busqueda(txteditarcodigo.get())
-        if variable1 !=None:
-            variable1.nombre=txteditarnombre.get()
-            variable1.prerrequisitos=txteditarprerrequisito.get()
-            variable1.obligatorio=txteditaropcionalidad.get()
-            variable1.semestre=txteditarsemestre.get()
-            variable1.creditos=txteditarcreeditos.get()
-            variable1.estado=txteditarestado.get()
-
-        messagebox.showinfo("Success","Se Editó el Curso")
+    global creditos
+    Bandera=True
+    for i in range( len(creditos)):
+        if (creditos[i].codigo)==txteditarcodigo.get():
+            variable1=Busqueda(txteditarcodigo.get())
+            if variable1 !=None:
+                variable1.nombre=txteditarnombre.get()
+                variable1.prerrequisitos=txteditarprerrequisito.get()
+                variable1.obligatorio=txteditaropcionalidad.get()
+                variable1.semestre=txteditarsemestre.get()
+                variable1.creditos=txteditarcreeditos.get()
+                variable1.estado=txteditarestado.get()
+                messagebox.showinfo("Success","Se Editó el Curso")
+                Bandera=False
+                return creditos
+    if Bandera==True and  (txteditarcodigo.get()!= "" and txteditarnombre.get()!="" and txteditarprerrequisito.get()!="" and txteditarsemestre.get()!="" and txteditaropcionalidad.get()!="" and txteditarcreeditos.get()!="" and txteditarestado.get()!=""):
+        messagebox.showinfo("Warning ","No se encontró el curso, intente con otro código")
     elif (txteditarcodigo.get()== "" and txteditarnombre.get()=="" and txteditarprerrequisito.get()=="" and txteditarsemestre.get()=="" and txteditaropcionalidad.get()=="" and txteditarcreeditos.get()=="" and txteditarestado.get()==""):
-        messagebox.showinfo("Warning","No Editó el Curso")
+        messagebox.showinfo("Warning ","Por favor llene los campos")
 
 def Busqueda(uno):
     global txteditarcodigo
@@ -609,15 +656,13 @@ def Busqueda(uno):
         if uno == hola.codigo:
             return hola
     return None
-
-
 def Principal():
     global VentanaP
     global botongestion
     #VENTANA
     VentanaP = tkinter.Tk()
     VentanaP.title("Practica 1")
-    VentanaP.geometry("900x600")
+    VentanaP.geometry("970x600")
     VentanaP.config(bg="sky blue")
     VentanaP.iconbitmap("favicon.ico")
     VentanaP.resizable(0,0)
@@ -628,6 +673,8 @@ def Principal():
     lblnombreestudiante.place(x=10,y=75)
     lblcarneestudiante = Label(VentanaP,text="Carné del Estudiante: 201902278",font="Cambria 22", fg="#9625b7", bg="sky blue")
     lblcarneestudiante.place(x=10,y=135)
+    lblseccion = Label(VentanaP,text="Sección: B+",font="Cambria 22", fg="black", bg="sky blue")
+    lblseccion.place(x=805,y=10)
     #BOTON PARA LA CARGA DEL ARCHIVO 
     botoncarga = Button(VentanaP,text="Cargar Archivo",font="Cambria 22", fg="gray", bg="#e4ade4", command=CargardeArchivo, relief="groove", bd=9, width=15)
     botoncarga.place(x=320,y=185)
@@ -641,6 +688,6 @@ def Principal():
     botonsalir = Button(VentanaP,text="Salir",font="Cambria 22", fg="gray", bg="#e4ade4", command=exit, relief="groove", bd=9,width="15")
     botonsalir.place(x=320,y=430)
     VentanaP.mainloop()
-    #ventana.destroy()
 Principal()
+
 
